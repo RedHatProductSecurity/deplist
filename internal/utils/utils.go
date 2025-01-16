5a82c76a51cb9edc2f00ee8f87a87da07a23b6c0
sfowl <sfowler@redhat.com>
2025-01-16 11:14:10 +1000

    Custom directory ignore (#40)
    * Custom directory ignore
    
    * add "test_scripts" to ignore dirs

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -5,22 +5,6 @@ import (
 	"path/filepath"
 )
 
-// BelongsToIgnoreList is fastest way we can do a string compare on a list
-func BelongsToIgnoreList(needle string) bool {
-	switch needle {
-	case
-		"node_modules",
-		"vendor",
-		"scripts",
-		"docs",
-		"test",
-		"tests",
-		".git":
-		return true
-	}
-	return false
-}
-
 func CharIsDigit(c string) bool {
 	if len(c) == 0 {
 		return false
